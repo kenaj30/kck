2fba3fd06b5fadcb5c741085d8fb3987b1a69176
kenaj30 <71976425+kenaj30@users.noreply.github.com>
2024-10-08 00:12:16 +0200

    Poprawione markery

--- kck1.py
+++ kck1.py
@@ -5,7 +5,7 @@ pliki = ['1ers.csv', '1crs.csv', '2crs.csv', '1c.csv', '2c.csv']
 tabele = ["" for _ in range(len(pliki))]
 kolory = ["b", "g", "r", "k", "m"] #https://matplotlib.org/stable/gallery/color/named_colors.html
 legenda = ["1-Evol-RS", "1-Coev-RS", "2-Coev-RS", "1-Coev", "2-Coev"]  # Uporządkowane legendy
-markery = ["o", "v", "d", "s", "D"] #https://matplotlib.org/stable/api/markers_api.html
+markery = ["o", "v", "D", "s", "d"] #https://matplotlib.org/stable/api/markers_api.html
 
 plt.figure(figsize=(8,8))
 
@@ -27,6 +27,7 @@ skala2 = plt.twiny()
 skala2.set_xticks([i*40 for i in range(6)])
 
 plt2=plt.subplot(1,2,2)
+
 tabela_box = [i.iloc[:, 2:].mean(axis=1)*100 for i in tabele]
 plt2.boxplot(tabela_box, showmeans = True, notch = True, boxprops = dict(color = 'blue'), meanprops = dict(marker = 'o', markerfacecolor = 'blue', markeredgecolor = 'blue'))
 #plt2.errorbar(ecolor= "blue")
