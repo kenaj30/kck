5593f29b1894de381dca7f379bdcafb3b2cab6f5
kenaj30 <71976425+kenaj30@users.noreply.github.com>
2024-10-08 21:26:20 +0200

    wersja final
    -zmiany kosmetyczne - czcionka, grubości, kolory
    -dodanie zapisu
    -zmiana logiki zbierania danych do boxplota z wszystkich wierszy na tylko ostatni - nie mam ochoty już żyć

--- kck1.py
+++ kck1.py
@@ -16,10 +16,10 @@ for i in range(len(pliki)):
     plt.plot(tabele[i].iloc[:, 1:2]/1000,tabele[i].iloc[:, 2:].mean(axis=1)*100, c = kolory[i], label = legenda[i], marker = markery[i],markevery = 25, markersize = 7, markeredgecolor="k")
 
 plt.grid(linestyle = "--")
-plt.margins(x=0) #y nie na 0
+#plt.margins(x=0) #y nie na 0, zamiana na xlim - jeden z plikow ma wiecej danych
 plt.title("Pokolenie")
-plt.xlabel(r"Rozeganych gier $\times$ 1000") #czy o to chodziło z czcionką?
-plt.ylabel(r"Odstek wygranych gier [%]")
+plt.xlabel(r"Rozeganych gier $\times$ 1000", fontsize = 11) #czy o to chodziło z czcionką?
+plt.ylabel(r"Odstek wygranych gier $[\%]$", fontsize = 11)
 plt.legend(loc = "lower right", numpoints = 2) #moze byc jeszcze loc = 4
 plt.ylim(60,100)
 plt.xlim(0,500)
@@ -28,20 +28,19 @@ skala2.set_xticks([i*40 for i in range(6)])
 
 plt2=plt.subplot(1,2,2)
 
-tabela_box = [i.iloc[:, 2:].mean(axis=1)*100 for i in tabele] #sprawdzic dane - niepoprane liczenie sredniej???
+tabela_box = [i.iloc[-1, 2:]*100 for i in tabele]
 plt2.boxplot(tabela_box, showmeans = True, notch = True, showfliers = True,
-             boxprops = dict(color = "blue"),
-             meanprops = dict(marker = "o", markerfacecolor = "blue", markeredgecolor = "blue"),
+             boxprops = dict(color = "blue", linewidth = 1.5),
+             medianprops = dict(color = "red", linewidth = 1.5),
+             meanprops = dict(marker = "o", markerfacecolor = "blue", markeredgecolor = "k"),
              whiskerprops = dict(color = "blue", linestyle = "--", linewidth= 1.5, dashes= (6, 6)),
              flierprops = dict(marker = "+", markeredgecolor = "blue")
              )
 plt2.grid(linestyle = "--")
-plt2.set_xticklabels(legenda, rotation = 20)
+plt2.set_xticklabels(legenda, rotation = 20, fontsize = 11)
 plt2.set_ylim(60,100)
 plt2.yaxis.tick_right()
 
-
-
-
 plt.tight_layout()
+#plt.savefig("kck1.png")
 plt.show()
