a2fee6e41c6ef674410665c584ae0bd0ee2db920
kenaj30 <71976425+kenaj30@users.noreply.github.com>
2024-10-16 14:08:08 +0200

    Zmiana przecięć skali
    -przeniesienie do środka

--- kck1.py
+++ kck1.py
@@ -4,7 +4,7 @@ import pandas as pd
 pliki = ['1ers.csv', '1crs.csv', '2crs.csv', '1c.csv', '2c.csv']
 tabele = ["" for _ in range(len(pliki))]
 kolory = ["b", "g", "r", "k", "m"] #https://matplotlib.org/stable/gallery/color/named_colors.html
-legenda = ["1-Evol-RS", "1-Coev-RS", "2-Coev-RS", "1-Coev", "2-Coev"]  # Uporządkowane legendy
+legenda = ["1-Evol-RS", "1-Coev-RS", "2-Coev-RS", "1-Coev", "2-Coev"]
 markery = ["o", "v", "D", "s", "d"] #https://matplotlib.org/stable/api/markers_api.html
 
 plt.figure(figsize=(8,8))
@@ -16,15 +16,17 @@ for i in range(len(pliki)):
     plt.plot(tabele[i].iloc[:, 1:2]/1000,tabele[i].iloc[:, 2:].mean(axis=1)*100, c = kolory[i], label = legenda[i], marker = markery[i],markevery = 25, markersize = 7, markeredgecolor="k")
 
 plt.grid(linestyle = "--")
-#plt.margins(x=0) #y nie na 0, zamiana na xlim - jeden z plikow ma wiecej danych
 plt.title("Pokolenie")
-plt.xlabel(r"Rozeganych gier $\times$ 1000", fontsize = 11) #czy o to chodziło z czcionką?
+plt.xlabel(r"Rozeganych gier $\times$ 1000", fontsize = 11)
 plt.ylabel(r"Odstek wygranych gier $[\%]$", fontsize = 11)
-plt.legend(loc = "lower right", numpoints = 2) #moze byc jeszcze loc = 4
+plt.legend(loc = "lower right", numpoints = 2)
 plt.ylim(60,100)
 plt.xlim(0,500)
 skala2 = plt.twiny()
 skala2.set_xticks([i*40 for i in range(6)])
+skala2.tick_params(axis='x', direction='in')
+plt.tick_params(axis='x', direction='in')
+plt.tick_params(axis='y', direction='in')
 
 plt2=plt.subplot(1,2,2)
 
@@ -40,6 +42,8 @@ plt2.grid(linestyle = "--")
 plt2.set_xticklabels(legenda, rotation = 20, fontsize = 11)
 plt2.set_ylim(60,100)
 plt2.yaxis.tick_right()
+plt2.tick_params(axis='x', direction='in')
+plt2.tick_params(axis='y', direction='in')
 
 plt.tight_layout()
 #plt.savefig("kck1.png")
